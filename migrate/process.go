560eafb9e6ce31871eebd96ff80776f431761461
feitian124 <feitian124@gmail.com>
2021-08-14 00:28:18 +0800

    should return migrated line

--- migrate/process.go
+++ migrate/process.go
@@ -9,8 +9,8 @@ import (
 	"strings"
 )
 
-// Walk 获取当前目录下的所有文件或目录信息
-func Walk(pwd string) {
+// ProcessFolder 获取当前目录下的所有文件或目录信息
+func ProcessFolder(pwd string) {
 	filepath.Walk(pwd, func(path string, info os.FileInfo, err error) error {
 		if !info.IsDir() {
 			if strings.HasSuffix(info.Name(), "_test.go") {
@@ -22,7 +22,7 @@ func Walk(pwd string) {
 	})
 }
 
-func ProcessFile(file string)  {
+func ProcessFile(file string) {
 	f, err := os.Open(file)
 	if err != nil {
 		log.Fatal(err)
@@ -51,50 +51,86 @@ func ProcessLine(line string) (string, error) {
 			if err != nil {
 				return "process equals has error", err
 			}
-			return r.expect + r.actual, nil
+			newLine := fmt.Sprintf("	require.Equal(%s, 2, %s)", r.expect, r.actual)
+			return newLine, nil
 		}
 		if strings.Contains(line, " DeepEquals") {
 			r, err := DeepEquals(line)
 			if err != nil {
 				return "process equals has error", err
 			}
-			return r.expect + r.actual, nil
+			newLine := fmt.Sprintf("	require.Equal(%s, 2, %s)", r.expect, r.actual)
+			return newLine, nil
 		}
 		if strings.Contains(line, " IsNil") {
 			r, err := IsNil(line)
 			if err != nil {
 				return "process equals has error", err
 			}
-			return r.expect + r.actual, nil
+			newLine := fmt.Sprintf("	require.Nil(t, %s)", r.actual)
+			return newLine, nil
 		}
 		if strings.Contains(line, " NotNil") {
 			r, err := NotNil(line)
 			if err != nil {
 				return "process equals has error", err
 			}
-			return r.expect + r.actual, nil
+			newLine := fmt.Sprintf("	require.NotNil(t, %s)", r.actual)
+			return newLine, nil
 		}
 		if strings.Contains(line, " IsTrue") {
 			r, err := IsTrue(line)
 			if err != nil {
 				return "process equals has error", err
 			}
-			return r.expect + r.actual, nil
+			newLine := fmt.Sprintf("	require.True(t, %s)", r.actual)
+			return newLine, nil
 		}
 		if strings.Contains(line, " IsFalse") {
 			r, err := IsFalse(line)
 			if err != nil {
 				return "process equals has error", err
 			}
-			return r.expect + r.actual, nil
+			newLine := fmt.Sprintf("	require.False(t, %s)", r.actual)
+			return newLine, nil
 		}
 		if strings.Contains(line, " Greater") {
 			r, err := Greater(line)
 			if err != nil {
 				return "process equals has error", err
 			}
-			return r.expect + r.actual, nil
+			newLine := fmt.Sprintf("	require.Greater(t, %s, %s)", r.actual, r.expect)
+			return newLine, nil
+		}
+		if strings.Contains(line, " LessEqual") {
+			r, err := LessEqual(line)
+			if err != nil {
+				return "process equals has error", err
+			}
+			newLine := fmt.Sprintf("	require.LessOrEqual(t, %s)", r.actual)
+			return newLine, nil
+		}
+		if strings.Contains(line, " GreaterEqual") {
+			r, err := GreaterEqual(line)
+			if err != nil {
+				return "process equals has error", err
+			}
+			newLine := fmt.Sprintf("	require.GreaterOrEqual(t, %s)", r.actual)
+			return newLine, nil
 		}
 	}
+
+	if strings.Contains(line, "func ") && strings.Contains(line, " Test") {
+		r, err := Function(line)
+		if err != nil {
+			return "process equals has error", err
+		}
+		if r.match {
+			newLine := fmt.Sprintf("func %s(t *testing.T) {", r.name)
+			return newLine, nil
+		}
+		return line, nil
+	}
+
 	return line, nil
 }
